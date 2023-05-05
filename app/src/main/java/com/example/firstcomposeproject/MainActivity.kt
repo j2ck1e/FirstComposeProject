ff8ecdec5ffe42262b408a4a9160f90be11b464f
j2ck1e <j2ck1e86@gmail.com>
2023-05-05 17:37:15 +0300

    Row, Box, Modifier

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -3,30 +3,62 @@ package com.example.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserInfo(name = "j2ck1e", age = 37)
+            TimesTable()
         }
     }
 }
 
 @Preview
 @Composable
-fun UserInfoPreview() {
-    UserInfo(name = "j2ck1e", age = 37)
-}
+fun TimesTable() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        for (i in 1 until 10) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                for (j in 1 until 10) {
+                    val color = if((i + j)%2 == 0) Color.Yellow else Color.White
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .border(1.dp, Color.DarkGray)
+                            .background(color),
+                        contentAlignment = Alignment.Center
 
-@Composable
-fun UserInfo(name: String, age: Int) {
-    Column {
-        repeat(10) { Text("Hello $name, you are $age years old") }
 
+                    ) {
+                        Text("${i * j}")
+                    }
+                }
+            }
+        }
     }
 }
