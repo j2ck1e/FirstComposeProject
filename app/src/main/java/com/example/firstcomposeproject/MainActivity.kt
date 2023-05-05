40e81f1baa23ff759e82e8448c0fa1d9185a2240
j2ck1e <j2ck1e86@gmail.com>
2023-05-05 17:00:56 +0300

    Preview and Column

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -3,14 +3,10 @@ package com.example.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,14 +17,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
 @Composable
-fun Greeting() {
-    val name = "Android"
-    Text(
-        text = "Hello $name!"
-    )
+fun UserInfoPreview() {
+    UserInfo(name = "j2ck1e", age = 37)
 }
+
 @Composable
-fun UserInfo(name: String, age: Int){
-    Text("Hello $name, you are $age years old")
+fun UserInfo(name: String, age: Int) {
+    Column {
+        repeat(10) { Text("Hello $name, you are $age years old") }
+
+    }
 }
