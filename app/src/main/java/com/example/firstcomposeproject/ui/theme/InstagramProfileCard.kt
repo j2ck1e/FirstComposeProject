7f879bb70d8c8a67a39b05ccd2c8f306b6faa7ee
j2ck1e <j2ck1e86@gmail.com>
2023-05-07 18:50:52 +0300

    Card

--- app/src/main/java/com/example/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/example/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -1,7 +1,7 @@
 package com.example.firstcomposeproject.ui.theme
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,9 +10,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,30 +23,31 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun InstagramProfileCard() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
+    Card(
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        colors = CardDefaults.cardColors(Color.White),
+        border = BorderStroke(1.dp, Color.Black)
     ) {
-        Box(
+        Row(
             modifier = Modifier
-                .size(50.dp)
-                .background(Color.Yellow)
-        )
-        TwoBoxes()
-        TwoBoxes()
-        TwoBoxes()
-
-
-
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(Color.Yellow)
+            )
+            TwoBoxes()
+            TwoBoxes()
+            TwoBoxes()
+        }
     }
-
 }
-
 @Composable
-private fun TwoBoxes(){
+private fun TwoBoxes() {
     Column(
         modifier = Modifier
             .height(80.dp),
