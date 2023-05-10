d0018731e107fadb5d76b5907ec544ca4cc29554
j2ck1e <j2ck1e86@gmail.com>
2023-05-10 23:37:25 +0300

    Image

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults.cardColors
@@ -15,7 +17,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
@@ -50,18 +56,13 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun TestText() {
-    Text(buildAnnotatedString {
-        withStyle(SpanStyle(fontWeight = FontWeight.ExtraBold)) {
-            append("Hello")
-        }
-        withStyle(SpanStyle(textDecoration = TextDecoration.Underline)) {
-            append(" ")
-        }
-        withStyle(SpanStyle(textDecoration = TextDecoration.LineThrough, fontSize = 24.sp)) {
-            append("World!")
-        }
-    })
+fun TestImage(){
+    Image(modifier = Modifier
+//        .fillMaxSize(),
+        .clip(CircleShape),
+        painter = painterResource(R.drawable.ic_instagram),
+        contentDescription = "",
+    contentScale = ContentScale.FillWidth)
 }
 
 
